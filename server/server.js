8a9dcd1fec552aace9f47ff2f21d24d5118f4f97
Sanjanadilhara <sanjanadilhara2000@gmail.com>
2023-11-26 13:16:27 +0530

    ws messaging

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var cookieParser = require('cookie-parser');
 const {MongoClient,  ObjectId}  = require('mongodb');
 const e = require('express');
 const cookieHandle=require('./cookieParse');
+const dataModule=require("./parseData");
 
 
 
@@ -16,7 +17,14 @@ const cookieHandle=require('./cookieParse');
 
 
 let onlineUsers=new Map();
+//consts
 
+const INIT_OK=3;
+const MSG_SENT_AND_RESPONSE_SENT=2;
+const MSG_SENT_RESPOSE_NOT_SENT=1
+const MSG_RECORDED=0;
+
+//consts
 
 
 
@@ -117,51 +125,128 @@ wss.on('connection', function connection(ws) {
   });
 
   ws.on('message', function message(data) {
-    let msgData;
-    try{
-      msgData=JSON.parse(data);
-    }catch{
-      return 1;
-    }
-    let result={msgId:msgData.msgId, stat:-1};
+    let msgData=new dataModule(data);
+    if(msgData.fail){console.log("fail");return;};
+
+    let result=new Object();
     let cookies=cookieHandle.parse(msgData.cookie);
 
     jwt.verify(cookies.auth, 'instmsg098', function(err, decoded){
       if(!err){
 
-          if(!(msgData?.init === undefined)){
+        let date=new Date();
+
+          if(msgData.type=="init"){
+
             onlineUsers.set(decoded.id, ws);
             console.log("user gone online "+onlineUsers.size);
             ws.uid=decoded.id;
+
+
+            result.type="init"
             result.stat=1;
             ws.send(JSON.stringify(result));
-          }else{
 
-            if(onlineUsers.has(msgData.to)){
-              onlineUsers.get(msgData.to).send(JSON.stringify({from:decoded.id, message:msgData.message}));
-              db.collection('messages').insertOne({from:new ObjectId(decoded.id), to:new ObjectId(msgData.to), message:msgData.message, sent:1}).then((res)=>{
-                result.stat=2;
-                ws.send(JSON.stringify(result));
-              }).catch((err)=>{
-                result.stat=-3;
-                ws.send(JSON.stringify(result));
+
+            db.collection("messages").find({to:new ObjectId(decoded.id), sent:MSG_RECORDED}).sort({date:1}).toArray()//send unsent messages
+            .then((data)=>{
+              data.forEach(element => {
+
+                ws.send(JSON.stringify({type:"message", from:element.from, message:element.message, date:element.date}));
+                sendResponse(element);
+
               });
+            }).catch((res)=>{});
+
+
+
+
+            db.collection("messages").find({from:new ObjectId(decoded.id), sent:MSG_SENT_RESPOSE_NOT_SENT}).sort({date:1}).toArray()//send unsent responses
+            .then((data)=>{
+              data.forEach(element => {
+
+                ws.send(JSON.stringify({type:"messageStat", msgId:element.msgId, stat:element.sent}));
+                //update the database message stat(sent);
+                db.collection("messages").updateOne({_id:element._id}, {$set:{sent:MSG_SENT_AND_RESPONSE_SENT}}).then((res)=>{
+                  console.log("msg Updated success")
+                })
+                .catch((res)=>{
+                  console.log("fail to update");
+                });
+      
+               });
+
+
+            }).catch((res)=>{});
+
+
+          }
+          else if(msgData.type=="message"){
+
+            let message=new dataModule.message(msgData);
+            if(message.fail){return;}
+            message.from=new ObjectId(decoded.id);
+            message.date=date;
+
+
+            if(onlineUsers.has(message.to.toString())){ //if user is online
+
+              onlineUsers.get(message.to.toString()).send(JSON.stringify({from:decoded.id, message:message.message, date:date}));
+
+              message.sent=MSG_SENT_AND_RESPONSE_SENT;
+             
+              console.log("message   "+message.msgId);
+
+              db.collection('messages').insertOne(message)
+              .then((res)=>{
+
+                result.type="messageStat";
+                result.stat=MSG_SENT_AND_RESPONSE_SENT;
+                result.msgId=message.msgId;
+                ws.send(JSON.stringify(result));
+
+              })
+              .catch((err)=>{
+
+                result.type="messageStat";
+                result.stat=-3;
+                result.msgId=message.msgId;
+                ws.send(JSON.stringify(result));
+
+
+              });
+
+
             }
-            else{
-              db.collection('messages').insertOne({from:new ObjectId(decoded.id), to:new ObjectId(msgData.to), message:msgData.message, sent:0}).then((res)=>{
-                result.stat=2;
+            else{   //if user is offline
+
+              message.sent=MSG_RECORDED;
+
+              db.collection('messages').insertOne(message).then((res)=>{
+                result.stat=MSG_RECORDED;
+                result.msgId=message.msgId;
+                result.type="messageStat"
                 ws.send(JSON.stringify(result));
               }).catch((err)=>{
+                result.type="messageStat"
+                result.msgId=message.msgId;
                 result.stat=-3;
                 ws.send(JSON.stringify(result));
               });
+
+
             }
 
 
           }
+          else if(msgData?.type=="request"){
+
+          }
+
           
         }else{
           result.stat=-2;
+          result.type="auth";
           ws.send(JSON.stringify(result));
         }
     });
@@ -171,11 +256,26 @@ wss.on('connection', function connection(ws) {
   // ws.send('something');
 });
 server.listen(80)
-// const httpServer=http.createServer((req, res)=>{
-//   res.writeHead(200, { 'Content-Type': 'text/plain' });
-// });
 
 
-// httpServer.listen(82, ()=>{
 
-// });
+function sendResponse(message){
+  if(onlineUsers.has(message.from)){
+    onlineUsers.get(message.from).send(JSON.stringify({type:"messageStat", msgId:message.msgId, stat:message.sent}));
+
+    db.collection("messages").updateOne({_id:message._id}, {$set:{sent:MSG_SENT_AND_RESPONSE_SENT}}).then((res)=>{
+      console.log("msg Updated success")
+    })
+    .catch((res)=>{
+      console.log("fail to update");
+    });
+  }
+  else{
+    db.collection("messages").updateOne({_id:message._id}, {$set:{sent:MSG_SENT_RESPOSE_NOT_SENT}}).then((res)=>{
+      console.log("msg Updated success")
+    })
+    .catch((res)=>{
+      console.log("fail to update");
+    });
+  }
+}
